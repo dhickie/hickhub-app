d625d45c4ea081f1ea928b73ed3b12ad2a9d477d
Dominic <dominichickie@gmail.com>
2018-03-02 19:08:12 +0000

    Add email check and registration endpoints to server

--- src/frontend/actions/registrationActions.js
+++ src/frontend/actions/registrationActions.js
@@ -85,37 +85,36 @@ export function securityBackClicked() {
 // Async action creators
 export function startEmailCheck(email) {
     return function(dispatch) {
-        dispatch(emailCheckSuccessful());
-        // dispatch(emailCheckStarted());
+        dispatch(emailCheckStarted());
 
-        // var body = JSON.stringify({
-        //     email
-        // });
-        // var options = {
-        //     method: 'POST',
-        //     body: body
-        // };
+        var body = JSON.stringify({
+            email
+        });
+        var options = {
+            method: 'POST',
+            body: body
+        };
 
-        // fetch('/registration/emailcheck', options)
-        // .then(response => {
-        //     if (response.status != 200) {
-        //         dispatch(emailCheckFailed(INTERNAL_EMAIL_ERROR));
-        //     } else {
-        //         // Handle the successful response
-        //         response.json()
-        //         .then(json => {
-        //             if (json.email_available) {
-        //                 dispatch(emailCheckSuccessful());
-        //             } else {
-        //                 dispatch(emailCheckFailed(EMAIL_IN_USE));
-        //             }
-        //         });
-        //     }
-        // },
-        // error => {
-        //     console.error('An error occured checking the email availability.', error);
-        //     dispatch(emailCheckFailed(INTERNAL_EMAIL_ERROR));
-        // });
+        fetch('/registration/emailcheck', options)
+        .then(response => {
+            if (response.status != 200) {
+                dispatch(emailCheckFailed(INTERNAL_EMAIL_ERROR));
+            } else {
+                // Handle the successful response
+                response.json()
+                .then(json => {
+                    if (json.email_available) {
+                        dispatch(emailCheckSuccessful());
+                    } else {
+                        dispatch(emailCheckFailed(EMAIL_IN_USE));
+                    }
+                });
+            }
+        },
+        error => {
+            console.error('An error occured checking the email availability.', error);
+            dispatch(emailCheckFailed(INTERNAL_EMAIL_ERROR));
+        });
     };
 };
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import request from 'request';
 import React from 'react';
 import RegistrationWindowContainer from './frontend/containers/registration/registrationWindow';
 import { createStore } from 'redux';
@@ -8,13 +10,111 @@ import { hickHubApp } from './frontend/reducers/root';
 import { LoginState } from './frontend/reducers/loginReducers';
 import { RegistrationState } from './frontend/reducers/registrationReducers';
 import { LoginWindow } from './frontend/components/loginWindow';
+import { access } from 'fs';
 
-var app = express();
 const port = 3100;
+const refreshBuffer = 10000;
 
-app.use('/static', express.static('./static'));
-app.use('/login', handleLogin);
-app.use('/register', handleRegister);
+var clientId = process.env.API_CLIENT_ID;
+var clientSecret = process.env.API_CLIENT_SECRET;
+
+var accessToken = '';
+var refreshToken = '';
+var accessTokenLifetime = 0;
+
+// Authenticate with the API
+authenticateClient(function() {
+    setupRefreshTimer(function() {
+        setupServer();
+    });
+});
+
+function setupRefreshTimer(callback) {
+    // Setup the timer which periodically refreshes the access token
+    if (accessTokenLifetime < refreshBuffer) {
+        setInterval(refreshWorker, accessTokenLifetime);
+    } else {
+        setInterval(refreshWorker, accessTokenLifetime - refreshBuffer);
+    }
+
+    // Call the callback
+    if (callback) {
+        callback();
+    }
+}
+
+function authenticateClient(callback) {
+    // Call the API to get a new access token/refresh token pair
+    var body = {
+        grant_type: 'client_credentials',
+        client_id: clientId,
+        client_secret: clientSecret,
+        scope: 'admin'
+    };
+    var options = {
+        url: 'https://api.hickhub.com/oauth/token',
+        method: 'POST',
+        form: body
+    };
+
+    request(options, function(error, response, body) {
+        if (error) {
+            console.error('An error occured trying to authenticate with the API: ' + error);
+        } else if (response.statusCode != 200) {
+            console.error('API responded to request with a non-OK status code: ' + response.statusCode);
+        } else {
+            // Extract the access token and refresh token
+            var resp = JSON.parse(body);
+            accessToken = resp.access_token;
+            refreshToken = resp.refresh_token;
+            accessTokenLifetime = resp.expires_in * 1000;
+
+            // Call the callback
+            if (callback) {
+                callback();
+            }
+        }
+    });
+}
+
+function refreshWorker() {
+    var body = {
+        grant_type: 'refresh_token',
+        client_id: clientId,
+        client_secret: clientSecret,
+        refresh_token: refreshToken
+    };
+    var options = {
+        url: 'https://api.hickhub.com/oauth/token',
+        method: 'POST',
+        form: body
+    };
+
+    request(options, function(error, response, body) {
+        if (error) {
+            console.error('An error occured trying to refresh the access token: ' + error);
+        } else if (response.statusCode != 200) {
+            console.error('API responded to request with a non-OK status code: ' + response.statusCode);
+        } else {
+            var resp = JSON.parse(body);
+            accessToken = resp.access_token;
+            refreshToken = resp.refresh_token;
+        }
+    });
+}
+
+function setupServer() {
+    var app = express();
+    app.use(bodyParser.json());
+    app.use('/static', express.static('./static'));
+    app.use('/login', handleLogin);
+    app.use('/register', handleRegister);
+
+    app.post('/registration/emailcheck', handleEmailCheck);
+    app.post('/registration/register', handleRegistrationRequest);
+
+    app.listen(port);
+}
 
 function handleLogin(req, res) {
     // Create the initial state based on the the query parameters
@@ -80,6 +180,57 @@ function handleRegister(req, res) {
     res.send(renderFullPage(html, finalState));
 }
 
+function handleEmailCheck(req, res) {
+    // Get the email we want to check
+    var email = req.body.email;
+
+    // Call the HickHub API to check whether it's available
+    var options = {
+        url: encodeURI(`https://api.hickhub.com/registration/email/${email}/available`),
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${accessToken}`
+        }
+    };
+
+    request(options, function(error, apiRes, body) {
+        if (error || apiRes.statusCode != 200) {
+            console.error(`An error occured checking whether the email was available: ${error}, status code: ${apiRes.statusCode}`);
+            res.sendStatus(500);
+        } else {
+            res.json(body);
+        }
+    });
+}
+
+function handleRegistrationRequest(req, res) {
+    // Call the HickHub API to register the user
+    var body = {
+        email: req.body.email,
+        password: req.body.password,
+        security_question: req.body.securityQuestion,
+        security_answer: req.body.securityAnswer
+    };
+    var options = {
+        url: 'https://api.hickhub.com/registration/user',
+        method: 'POST',
+        body: body,
+        json: true,
+        headers: {
+            Authorization: `Bearer ${accessToken}`
+        }
+    };
+
+    request(options, function(error, apiRes, body) {
+        if (error || apiRes.statusCode != 200) {
+            console.error(`An error occured registering the user: ${error}, status code: ${apiRes.statusCode}`);
+            res.sendStatus(500);
+        } else {
+            res.json(body);
+        }
+    });
+}
+
 function renderFullPage(html, preloadedState) {
     return (
         `
@@ -100,6 +251,3 @@ function renderFullPage(html, preloadedState) {
         `
     );
 }
-
-app.listen(port);
-
